a6760e5e50434bbc45b27ab19ba8b19bc6fee3d6
Altus Carstens <carstensaltus@gmail.com>
2020-05-30 13:41:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SSL Certificate
+
+```sh
+module "example" {
+    source = "git@gitlab.woopidi.net:terraform/cloudfront-s3.git"
+    domain_name = "app.example.com"
+    zone_id = "123456"
+}
+
+```
---
+++ certificate.tf
@@ -0,0 +1,32 @@
+# Create the certificate in the Virgina Region
+resource "aws_acm_certificate" "certificate" {
+  domain_name               = var.domain_name
+  validation_method         = "DNS"
+  provider                  = aws.virginia
+  subject_alternative_names = ["*.${var.domain_name}"]
+
+  tags = {
+    terraform = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create a rDNS record to validate the certificate 
+resource "aws_route53_record" "certificate_record" {
+  zone_id = var.zone_id
+  ttl     = 60
+  name    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value]
+}
+
+# Validate the certificate
+resource "aws_acm_certificate_validation" "validate" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  provider                = aws.virginia
+  validation_record_fqdns = [aws_route53_record.certificate_record.fqdn]
+}
+
